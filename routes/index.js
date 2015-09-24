a6471fa3c4f834c9286d741c850ba8a0e0ac3eed
Shlomi Haver <shlomih@appollocretive.com>
2015-09-25 01:57:30 +0300

    create cache on app create

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var jsonData = require('../data/sample_data.json');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+
+router.get('/data', function (req, res, next) {
+    var fromData = req.query["fromData"],
+    toData = req.query["toData"];
+
+var a =jsonData;
+
+    if(toData === fromData){
+        //hourly
+
+
+    }else{
+        //daily
+    }
 });
 
 module.exports = router;
